0022e68e10c0a07f0dd395078dbefa5188f01877
갑열 주 <cjswoduf@gmail.com>
2022-12-23 15:52:40 +0900

    Fix: 초기 셋팅 수정

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { studyRouter, userRouter } = require('./routers')
+const path = require('path');
+const morgan = require('morgan');
+
+
+const { sequelize } = require('./db');
+
+const app = express();
+
+
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/users', userRouter);
+
+
+module.exports = { app }
