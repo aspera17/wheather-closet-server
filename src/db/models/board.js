5009be41964b37385abd0d3ad8e90688b55f85b1
[코치] 이재원 <jae_won@icloud.com>
2022-12-27 16:10:22 +0000

    Update board.js

--- src/db/models/board.js
+++ src/db/models/board.js
@@ -1,41 +1,34 @@
-module.exports = (sequelize, DataTypes) => {
-    const Board = sequelize.define('Board', {
-      id: {
-        type: DataTypes.INTEGER.UNSIGNED,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      clothes_style: {
-        type: DataTypes.ENUM('정장', '캐쥬얼', '스트릿'),
-        allowNull: false,
-      },
-      board_address_id: {
-        type: DataTypes.INTEGER.UNSIGNED,
-        allowNull: false,
-      },
-      user_id: {
-        type: DataTypes.INTEGER.UNSIGNED,
-        allowNull: false,
-      }
-      }, {
+const {DataTypes} = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('board', {
+        id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        clothes_style: {
+            type: DataTypes.ENUM('정장', '캐쥬얼', '스트릿'),
+            allowNull: false,
+        },
+        board_address_id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+        }
+    }, {
         sequelize,
-        timestamps: true, 
-        underscored: true, 
+        timestamps: true,
+        updatedAt: false,
+        deletedAt: false,
+        createdAt: true,
+        underscored: true,
         freezeTableName: true,
-        paranoid: true, //soft delete
         charset: 'utf8',
         collate: 'utf8_general_ci',
-      });
-
-        Board.associate = (models) => {
-
-        Board.hasMany(models.Board_like, {foreignKey: 'board_id', sourceKey: 'id'});
-        Board_like.belongsTo(models.Board, {onDelete:'set null'});
-        Board.hasMany(models.Board_tag, {foreignKey: 'board_id', sourceKey: 'id'});
-        Board_tag.belongsTo(models.Baord, {onDelete:'cascade'});
-        Board.hasOne(models.Board_image, {foreignKey: 'board_id', sourceKey: 'id'});
-        Board_image.belongsTo(models.Board, {onDelete:'cascade'});
-      };
-        return Board;
-    };
+    })
+};
