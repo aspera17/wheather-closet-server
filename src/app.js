87a167d6e29c9a455ed23da1396d61ba119928ed
[코치] 이재원 <jae_won@icloud.com>
2022-12-27 16:10:03 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,25 +1,16 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
-const { boardRouter, userRouter } = require('./routers')
-const { sequelize } = require('./db');
+const {boardRouter, userRouter} = require('./routers')
 const app = express();
 
-
-sequelize.sync({ force: false })
-  .then(() => {
-    console.log('데이터베이스 연결 성공');
-  })
-  .catch((err) => {
-    console.error(err);
-  });
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
 app.use('/api/user', userRouter)
 app.use('/api/board', boardRouter)
 
-module.exports = { app }
+module.exports = {app}
