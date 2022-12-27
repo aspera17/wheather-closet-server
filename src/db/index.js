866dafc419d91acd931664252a8dc14ef05f569f
[코치] 이재원 <jae_won@icloud.com>
2022-12-27 16:10:40 +0000

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,23 +1,17 @@
-//models/index.js
-const Sequelize = require('sequelize');
-const {User} = require('./models')
+const {Sequelize} = require('sequelize');
+
 const env = process.env.NODE_ENV || 'development';
 const config = require('../../config/config')[env];
-const db = {};
-
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
 
-db.User = User;
+const modelDefiners = [
+    require('./models/board'),
+];
 
+// We define all models according to their files.
+for (const modelDefiner of modelDefiners) {
+    modelDefiner(sequelize);
+}
 
-// User.init(sequelize);
-
-
-// User.associate(db)
-
-
-
-module.exports = db;
+module.exports = sequelize;
