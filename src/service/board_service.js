562980e4ff35e39a0433f235891d5331cb8c54b7
갑열 주 <cjswoduf@gmail.com>
2022-12-27 23:05:20 +0900

    feat: board router 및 service 로직 작성중

--- src/service/board_service.js
+++ src/service/board_service.js
@@ -1,5 +1,7 @@
-// const { ?? } = require("../db");
-// const { ?? } = require("../db/models");
+const { Board } = require("../db");
+// const { Board } = require("../db/models");
+const Op = Sequelize.Op;
+
 
 class BoardService {
 //     constructor(study_model, recruit_model, like_model, study_tag_model) {
@@ -8,24 +10,38 @@ class BoardService {
 //       this.Recruit = recruit_model;
 //       this.Like = like_model;
 //     } 
+
 // 1. 게시물 등록
-  //  async addBoard
+//  async createBoard() {}
 
 // 2. 기본 게시물 조회 (시간 순서)
     async getBoardByTime(){
-        return [{"title": "asdads"}];
+        // board 테이블에서 created_at 순서대로 가져온 다음
+        // board에 대한 정보를 반환한다.
+        // SELECT * FROM board ORDER BY `created_at` asc LIMIT 100;
+
+        return Board.findAll({
+            order: ['createdAt', 'DESC']
+        });
     }
+
 // 3. 게시물 조회 (좋아요 순서)
     async getBoardByLike() {
-
+        // board_like 테이블의 board_id 칼럼 중
+        // 각 id의 count를 세서 높은 순서대로 가져온다
+        // board_id가 동일할 떄 count 숫자 - unklike의 숫자  
     }
 // 4. 게시물 조회 (거리가 가까운 순서)
     async getBoardByDistance() {
-
+        // param? 에 담긴 gps 값을 이용해서
+        // 가까운 순서대로 가져온다.
+        // 전제: 사용자가 동의하지 않았으면 
+        // env 파일의 default 위도 경도 값을 기준으로 가져와야함.
     }
 // 5. 게시물에 좋아요 누르기
 
 // 6. 게시물에 좋아요 취소하기
+
 // 7. 게시물 삭제하기
 };
 
